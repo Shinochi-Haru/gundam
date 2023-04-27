dec2019c17800e642a3d32b60770f5ac0b67dfd9
Shinochi-Haru <haru060109011305@gmail.com>
2023-04-27 23:03:21 +0900

    [add] 水平方向の動き

--- Assets/Script/MovingController.cs
+++ Assets/Script/MovingController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class MovingController : MonoBehaviour
 {
     [SerializeField] float _upPower = 10f;
+    [SerializeField] float _horizonPower = 10f;
+    private Vector3 moveDirection; // キャラクターの移動方向
+
     Rigidbody _rb;
     void Start()
     {
@@ -19,6 +22,22 @@ public class MovingController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // 上下左右のキーの入力を取得
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        // 移動する方向を決める
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        // 移動する向きにキャラクターの向きを変える
+        if (direction.magnitude >= 0.1f)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 10f);
+        }
+
+        // Rigidbodyに力を加える
+        Vector3 movement = direction * _horizonPower * Time.deltaTime;
+        _rb.AddForce(movement, ForceMode.VelocityChange);
         //上昇する
         if (Input.GetKey(KeyCode.Space))
         {
